d757d8967b1dd53e417b8a8bc16b7811a96d5133
Nadir <mail.pour.dev@gmail.com>
2025-12-29 01:19:12 +0100

    refactor: Centralize Bazel Helm chart definition and parameterize deployment image, and add a delay to the Python app.

--- python/hello_world/main.py
+++ python/hello_world/main.py
@@ -1,8 +1,13 @@
 import requests
+import time
 
 def hello():
+    print("Waiting for 5 seconds...")
+    time.sleep(5)
     return "Hello from Python!"
 
 if __name__ == "__main__":
     print(hello())
+    print("Waiting for 5 seconds...")
+    time.sleep(5)
     print(f"requests version: {requests.__version__}")
